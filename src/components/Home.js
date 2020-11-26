da9d098f3ed102f588f5a97f7a9b76091e8be4c5
DuaaH <dodohalaiqa.1@gmail.com>
2020-11-26 15:12:04 +0200

    added some style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import data from '../../data.json'
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
 
 function Home() {
   const [productss, setproducts] = useState(data.products)
-  console.log("p: ", productss);
   return (
-
     <ScrollView >
-      {productss.map((item, index) => {
-        return <Card>
-          <Card.Image source={{ uri: item.picture }} />
-          <Card.Title>{item.name}</Card.Title>
-          <Card.FeaturedTitle style={{ color: 'black' }}>{item.price}</Card.FeaturedTitle>
-          <Button
-            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-            title='VIEW NOW' />
-        </Card>
+      <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
+        {productss.map((item, index) => {
+          return <View key={item.id} style={{ width: '50%' }}>
+            <Card>
+              <Card.Image source={{ uri: item.picture }} />
+              <View style={{ flex: 1, flexDirection: 'column' }}>
+                <Card.FeaturedTitle style={{ color: 'black', fontSize: 14 }}>{item.name}</Card.FeaturedTitle>
+                <Card.FeaturedTitle style={{ color: 'black', fontSize: 14 }}>{item.price}</Card.FeaturedTitle>
+              </View>
 
-      })}
+              <TouchableOpacity style={{
+                elevation: 8,
+                backgroundColor: "#013A20",
+                borderRadius: 10,
+                paddingVertical: 10,
+                paddingHorizontal: 10
+              }}>
+                <Text style={{
+                  fontSize: 18,
+                  color: "#fff",
+                  fontWeight: "bold",
+                  alignSelf: "center",
+                  textTransform: "uppercase"
+                }}>Details</Text>
+              </TouchableOpacity>
+            </Card>
+          </View>
+        })}
+      </View>
     </ScrollView>
   );
 }
