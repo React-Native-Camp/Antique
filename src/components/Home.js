fa8742d2367e596f94e7d45467b7bbd9b653961b
DuaaH <dodohalaiqa.1@gmail.com>
2020-12-01 16:36:47 +0200

    made the search bar accept input from the user

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,25 @@ import ProductCard from './ProductCard'
 
 function Home() {
   const [productss, setproducts] = useState(data.products)
+  const [input, setinput] = useState('')
+
+  const updateInput = (e) => {
+    setinput(e);
+  }
+
   return (
     <ScrollView >
       <SearchBar
         round
         searchIcon={{ size: 25 }}
         placeholder="Type Here......"
+        onChangeText={updateInput}
+        value={input}
       />
       <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
-        {productss.map((item, index) => {
+        {input ? data.products.filter(e => e.name.includes(input)).map((item, index) => {
+          return <ProductCard key={item.id} item={item} />
+        }) : productss.map((item, index) => {
           return <ProductCard key={item.id} item={item} />
         })}
       </View>
